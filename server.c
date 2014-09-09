af78aa80c3bf3c3e2d936696bf5609b42ae3847e
zhaopeng <sczhaopeng@gmail.com>
2014-09-09 13:18:40 +0800

    add server

---
+++ server.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: server.c
+	> Author: zp
+	> Mail: sczhaopeng@gmail.com 
+	> Created Time: Tue 09 Sep 2014 01:17:50 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main(int argc,char **argv)
+{
+
+
+}
